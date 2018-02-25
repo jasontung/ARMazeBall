1565e909fb8e4f65953dac512017fd4257f59f27
jasontung2214 <jasontung2214@gmail.com>
2018-02-25 21:44:26 +0800

    Initial

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+
+    private Vector3 startPos;
+    [ContextMenu("Reset Position")]
+    private void ResetPosition()
+    {
+        transform.localPosition = startPos;
+    }
+
+    private void Start()
+    {
+        startPos = transform.localPosition;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.localPosition = startPos;
+    }
+}
---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraHandler : MonoBehaviour {
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+        }
+    }
+   
+}
