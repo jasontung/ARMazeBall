828ef699f6159ae8bc74a4e81158bd9a2f1e8414
jasontung2214 <jasontung2214@gmail.com>
2018-04-18 01:46:19 +0800

    no message

---
+++ Assets/Scripts/SummonController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SummonController : MonoBehaviour {
+    private TouchController touchController;
+    public float summonTime = 5f;
+    public SummomSetting[] summonSettings;
+    private GameObject summonStuff;
+    public Button nextButton;
+
+    private void Awake()
+    {
+        touchController = GameObject.FindObjectOfType<TouchController>();
+        touchController.onTouchCounterFinish += OnSummonOver;
+        nextButton.gameObject.SetActive(false);
+    }
+
+    public void OnSummonStart ()
+    {
+        if (summonStuff)
+            Destroy(summonStuff);
+        nextButton.gameObject.SetActive(false);
+        touchController.StartTouch(summonTime);
+	}
+	
+    public void OnSummonLost()
+    {
+        touchController.Reset();
+    }
+    
+	public void OnSummonOver(int power)
+    {
+        int usingIndex = 0;
+        for(int i = 0; i < summonSettings.Length; i++)
+        {
+            if (power >= summonSettings[i].requirePower)
+                usingIndex = i;
+        }
+        summonStuff = Instantiate(summonSettings[usingIndex].prefab);
+        nextButton.gameObject.SetActive(true);
+    }
+}
+
+[System.Serializable]
+public class SummomSetting
+{
+    public GameObject prefab;
+    public int requirePower;
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+public class TouchController : MonoBehaviour
+{
+    public int touchCount = 0;
+    public event Action<int> onTouchCounterFinish;
+    public Transform touchEffectsRoot;
+    public Slider timeBar;
+    private ParticleSystem[] allTouchEffects;
+    private void Awake()
+    {
+        allTouchEffects = new ParticleSystem[touchEffectsRoot.childCount];
+        foreach(Transform child in touchEffectsRoot)
+        {
+            allTouchEffects[child.GetSiblingIndex()] = child.GetComponent<ParticleSystem>();
+        }
+        Reset();
+    }
+
+    public void Reset()
+    {
+        timeBar.gameObject.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    public void StartTouch(float duration)
+    {
+        touchCount = 0;
+        StartCoroutine(DoTouchCounter(duration));
+    }
+
+    IEnumerator DoTouchCounter(float duration)
+    {
+        timeBar.gameObject.SetActive(true);
+        timeBar.maxValue = duration;
+        timeBar.value = timeBar.maxValue;
+        while (duration > 0)
+        {
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0))
+            {
+                touchCount++;
+                PlayTouchEffect();
+            }
+#else
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+                    touchCount++;
+                    PlayTouchEffect();
+                }
+            }
+#endif
+            yield return null;
+            duration -= Time.deltaTime;
+            timeBar.value = duration;
+        }
+        if (onTouchCounterFinish != null)
+            onTouchCounterFinish.Invoke(touchCount);
+    }
+
+    private void PlayTouchEffect()
+    {
+        int index = UnityEngine.Random.Range(0, allTouchEffects.Length);
+        allTouchEffects[index].Play();
+    }
+}
